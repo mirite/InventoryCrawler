a19582586788d5f997ad3f82536cac9f0c65ac87
Jesse Conner <jconner@ad.hutchinsonfarmsupply.com>
2020-04-16 10:02:11 -0400

    Strip away unnecessary info

--- filter.py
+++ filter.py
@@ -7,5 +7,11 @@ with open('find-a.csv', 'r') as f_list:
     reader = csv.reader(f_list)
 
     for url in reader:
-        if(re.search('\d{5,10}$',url[0])):
-            print(url[0])
+        url=url[0]
+        if(re.search('\d{5,10}$',url)):
+            url=url.replace('/inventory/v1/Current/','')
+            url=url.replace('/',',')
+            url=url.replace('-',' ')
+            url=url.replace('  Stouffville Ontario   ',',')
+            url=url.replace('Farm Equipment,','')
+            print(url)
