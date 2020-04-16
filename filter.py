64160fc376e94d7dd106ca6eead9fa27c0af4d6e
Jesse Conner <jconner@ad.hutchinsonfarmsupply.com>
2020-04-16 10:27:29 -0400

    Output as JSON

--- filter.py
+++ filter.py
@@ -2,16 +2,27 @@ import csv
 import re
 
 tofilter=[]
+out=''
 
 with open('find-a.csv', 'r') as f_list:
     reader = csv.reader(f_list)
-
+    out='['
     for url in reader:
         url=url[0]
-        if(re.search('\d{5,10}$',url)):
+        
+        if(re.search('\d{7,10}$',url)):
             url=url.replace('/inventory/v1/Current/','')
             url=url.replace('/',',')
             url=url.replace('-',' ')
             url=url.replace('  Stouffville Ontario   ',',')
             url=url.replace('Farm Equipment,','')
-            print(url)
+            listing=url.split(',')
+            trimid=listing[len(listing)-1]
+            modeltrim=listing[len(listing)-2]
+            #print(listing)
+            out=out+'{"trimid":' + trimid + ',"modeltrim":"'+modeltrim + '"},'
+out=out[:-1] + ']'
+#print(out)
+with open('trims.json', 'w') as f_list:
+    f_list.write(out)
+print('Done!')
