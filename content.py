776cf8f83a09d24b5ed8c0fe9010af5a182862ed
Jesse Conner <jesse@rootree.ca>
2020-11-03 12:39:31 -0500

    Simplify output from shogun

--- content.py
+++ content.py
@@ -37,13 +37,62 @@ if(path.exists(page_list)):
 else:
   sys.exit("Page list missing for " + site_name + " please recrawl")
 
+if not os.path.exists(site_name + '/converted'):
+  os.makedirs(site_name + '/converted')
+
+total_changes = 0
+
 #Loop through all pages
 for page in pages:
 
     cache_path = site_name + "/cache/" + page['title'] + ".dat"
     with open(cache_path, "r") as cache_file:
+
         content = cache_file.read()
         soup = BeautifulSoup(content, 'html.parser')
-        for a in soup.find_all('div'):
-            print(str(a))
-print("Done!")
+
+        
+        
+        #Only deal with pages with shogun content
+        for a in soup.find_all("div", "shogun-root"):
+
+          title = page['title']
+          title = title.replace("httpsrootreedotca","") #fix hardcode value
+
+          
+
+          print("Converting page ", title)
+
+          #Remove link tags and scripts from the page
+          [s.extract() for s in a('script')]
+          [s.extract() for s in a('link')]
+
+          changes = 0
+
+          while (True):
+
+            previous_changes = changes
+
+            for d in a('div'):
+              if(len(d.contents) == 0):
+                d.extract()
+                changes = changes + 1
+            
+            for s in a('span'):
+              if(len(s.contents) == 0):
+                s.extract()
+                changes = changes + 1
+            
+            #Only leave loop if there were no additional changes
+            if(changes == previous_changes):
+              break
+
+          total_changes = total_changes + changes
+
+          converted_path = site_name + "/converted/" + title + ".html"
+
+          with open(converted_path,"w") as output:
+
+            output.write(a.prettify(formatter="minimal"))
+
+print("Done! ", str(total_changes), " empty elements removed")
