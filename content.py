f963f73cc5670b40a3925e0d052746caf3b84477
Jesse Conner <jesse@rootree.ca>
2020-11-09 14:24:38 -0500

    Pretty up code and some basic error handling

--- content.py
+++ content.py
@@ -1,3 +1,6 @@
+########
+# Imports
+#######
 import json
 
 #File/operating system related imports
@@ -14,37 +17,11 @@ import urllib3
 
 from bs4 import BeautifulSoup as BeautifulSoup #Used for page navigation
 
-#Make sure SSL is configured
-http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+########
+# Helper Functions
+#######
 
-site_name = "rootree"
-
-#Load the site info from the crawler
-config_path = site_name + '/info.json'
-
-if(path.exists(config_path)):
-    info=json.load(open(config_path))
-else:
-    sys.exit("Site config not found")
-
-domain = info['path']
-stamp = info['created']
-
-page_list = site_name + '/pages.json'
-
-if(path.exists(page_list)):
-  pages=json.load(open(page_list))
-else:
-  sys.exit("Page list missing for " + site_name + " please recrawl")
-
-if not os.path.exists(site_name + '/converted'):
-  os.makedirs(site_name + '/converted')
-
-total_changes = 0
-singles = 0
-
-#Loop through all pages
-
+#Remove unwanted attributes from parent tag
 def strip_meta(a):
   del a['class']
   del a['id']
@@ -57,84 +34,132 @@ def strip_meta(a):
   del a['data-shogun-variant-id']
   del a['data-col-grid-mode-on']
 
+
+#Load the site info from the crawler
+def load_config(site_name):
+  
+  config_path = site_name + '/info.json'
+
+  if(path.exists(config_path)):
+      info=json.load(open(config_path))
+  else:
+      sys.exit("Site config not found")
+
+  return info
+
+
+#Returns the list of pages to convert
+def get_pages(config):
+  page_list = site_name + '/pages.json'
+
+  if(path.exists(page_list)):
+    pages=json.load(open(page_list))
+  else:
+    sys.exit("Page list missing for " + site_name + " please re-crawl")
+
+  if not os.path.exists(site_name + '/converted'):
+    os.makedirs(site_name + '/converted')
+  return pages
+
+
+########
+# Variables and config
+#######
+
+#Make sure SSL is configured
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+
+site_name = "rootree"
+domain = ""
+stamp = ""
+total_changes = 0
+singles = 0
+
+########
+# Main body
+#######
+
+
+print("\n##########\n# Rootree Shogun Tool\n##########")
+
+#Load the site info from the crawler
+config = load_config(site_name)
+pages = get_pages(config)
+
+print("Converting pages from",config['path'],"crawled on",config['created'],"\n")
+
+#Loop through all pages
 for page in pages:
 
-  cache_path = site_name + "/cache/" + page['title'] + ".dat"
-  with open(cache_path, "r") as cache_file:
+  try:
+    cache_path = site_name + "/cache/" + page['title'] + ".dat"
+    with open(cache_path, "r") as cache_file:
 
-      content = cache_file.read()
-      content=content.replace('style=""',"")
+        content = cache_file.read()
+        content=content.replace('style=""',"")
 
-      soup = BeautifulSoup(content, 'html.parser')
+        soup = BeautifulSoup(content, 'html.parser')
 
-      passes = 0 #Right now I'm doing three passes with no changes
-      
-      #Only deal with pages with shogun content
-      for a in soup.find_all("div", "shogun-root"):
-
-        title = page['title']
-        title = title.replace("httpsrootreedotca","") #fix hardcode value
+        passes = 0 #Right now I'm doing three passes with no changes
         
-        strip_meta(a)
+        #Only deal with pages with shogun content
+        for a in soup.find_all("div", "shogun-root"):
 
-
-        
-
-        print("Converting page ", title)
-
-        #Remove link tags and scripts from the page
-        [s.extract() for s in a('script')]
-        [s.extract() for s in a('link')]
-
-        changes = 0
-
-        while (True):
-
-          previous_changes = changes
-
-          for d in a('div'):
-
-            strip_meta(d)
-
-            child_count = len(list(d.findChildren("div", False)))
-            print(str(child_count) + " children. ") #+ str(d.contents))
-
-            if(child_count < 5):
-              for child in d.findChildren("div", False):
-                print("Child: " + str(child.contents) + "\n\n")
-
-            if(len(d.contents) == 0):
-              d.extract()
-              changes = changes + 1
-            elif(child_count == 1):
-              d.unwrap()
-              #print(str(d.children))
-              #d.replace_with(list(d.children)[0])
-              changes = changes + 1
-              singles = singles + 1
-
-
+          title = page['title']
+          title = title.replace("httpsrootreedotca","") #fix hardcode value
           
-          for s in a('span'):
-            if(len(s.contents) == 0):
-              s.extract()
-              changes = changes + 1
-          
-          #Only leave loop if there were no additional changes
-          if(changes == previous_changes and passes > 2):
-            passes = 0
-            break
-          elif(changes == previous_changes):
-            print("Finished pass " + str(passes + 1))
-            passes = passes + 1
+          print("Converting page", title)
 
-        total_changes = total_changes + changes
+          #Remove link tags and scripts from the page
+          [s.extract() for s in a('script')]
+          [s.extract() for s in a('link')]
+          strip_meta(a)
 
-        converted_path = site_name + "/converted/" + title + ".html"
+          changes = 0
+          previous_changes = -1 #If we default to zero the loop won't run
 
-        with open(converted_path,"w") as output:
+          while (changes != previous_changes):
 
-          output.write(a.prettify(formatter="minimal"))
-  break #Uncomment to test only first file for debug
+            previous_changes = changes
 
-print("Done! ", str(total_changes), " change made. ", str(singles), " single children divs eliminated")
+            for d in a('div'):
+
+              strip_meta(d)
+
+              child_count = len(list(d.findChildren("div", False)))
+              #print(str(child_count) + " children. ") #+ str(d.contents))
+
+              #if(child_count < 5):
+              #  for child in d.findChildren("div", False):
+              #    print("Child: " + str(child.contents) + "\n\n")
+
+              if(len(d.contents) == 0):
+                d.extract()
+                changes = changes + 1
+              elif(child_count == 1):
+                d.unwrap()
+                #print(str(d.children))
+                #d.replace_with(list(d.children)[0])
+                changes = changes + 1
+                singles = singles + 1
+
+
+            
+            for s in a('span'):
+              if(len(s.contents) == 0):
+                s.extract()
+                changes = changes + 1
+            
+          total_changes = total_changes + changes
+
+          converted_path = site_name + "/converted/" + title + ".html"
+
+          with open(converted_path,"w") as output:
+            output.write(a.prettify(formatter="minimal"))
+
+    #break #Uncomment to test only first file for debug
+  except Exception as e:
+    print("Error processing",page['title'],e)
+
+
+print("\nDone!", str(total_changes), "change made.", str(singles), "single child divs eliminated.\n")
